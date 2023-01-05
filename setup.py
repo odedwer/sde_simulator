28f70d970ef59859dfba8c389094857bb315c6e9
odedwer <O1d9e2d8>
2023-01-05 10:18:25 +0200

    fixed version

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 
 setup(
     name='sde_simulator',
-    version='0.1.7 description="General base class implementation of Rung-Kutta SDE solver"',
+    version='0.1.7',
+    description="General base class implementation of Rung-Kutta SDE solver",
     long_description="This package provides a base class for simulating SDE using Rung-Kutta method.\n"
                      "The base class implements the simulation and requires the override of the deterministic and "
                      "stochastic methods of the model.",
